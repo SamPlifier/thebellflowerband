7ae7094bc47b80f37dd9ccd671b16a6a60e9de58
Sam Davis-Castro <sam.davis-castro@blueacornici.com>
2022-03-12 23:01:45 -0500

    2

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/media":["static\u002Fchunks\u002Fpages\u002Fmedia.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fmedia"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
